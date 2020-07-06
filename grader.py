5c707631759125f454574a6165a133796ab002d5
Pablo Trinidad <p@pablotrinidad.me>
2020-07-06 18:24:40 -0500

    Added grader

--- grader.py
+++ grader.py
@@ -1,8 +1,32 @@
 """PS05 Grader."""
 
+import sys
+
 from solution import Set
 
 
 if __name__ == '__main__':
-    data = Set()
-    print("\t✅ Test case accepted")
+    name, n = input(), int(input())
+    got, want, ops = [None] * n, [None] * n, [None] * n
+    ans, sol = set(), Set()
+    for i in range(n):
+        l = input().split(' ')
+        op, v = l[0], int(l[1])
+        if l[0] == 'A':
+            want[i], got[i] = ans.add(v), sol.add(v)
+            ops[i] = f'add({v})'
+        elif l[0] == 'R':
+            want[i], got[i] = ans.discard(v), sol.remove(v)
+            ops[i] = f'remove({v})'
+        else:
+            want[i], got[i] = v in ans, sol.contains(v)
+            ops[i] = f'contains({v})'
+
+    if got != want:
+        print(f"\t❌ Test case #{name} failed")
+        if n < 50:
+            print(f"\t\tOps:\t{ops}")
+            print(f"\t\tGot:\t{got}")
+            print(f"\t\tWant:\t{want}")
+        sys.exit(1)
+    print(f"\t✅ Test case accepted #{name}")
--- solution.py
+++ solution.py
@@ -6,13 +6,16 @@ class Set:
 
     def __init__(self):
         """Initializes the set."""
+        self.data = set()
 
     def add(self, k: int) -> None:
         """Add k into the set. If k already exists in the set, do nothing."""
+        self.data.add(k)
 
     def remove(self, k: int) -> None:
         """Removes k from the set if exists, else do nothing."""
+        self.data.discard(k)
 
     def contains(self, k: int) -> bool:
         """Returns whether k is present in the set or not."""
-        return False
+        return k in self.data
